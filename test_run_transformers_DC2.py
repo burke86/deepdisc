7e45ee25d52eef4d1bd39f1498456515a7d896e1
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-10-31 10:18:59 -0400

    Removal unused imports

--- test_run_transformers_DC2.py
+++ test_run_transformers_DC2.py
@@ -14,57 +14,25 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 # Some basic setup:
 # Setup detectron2 logger
-import detectron2
 from detectron2.utils.logger import setup_logger
 
 setup_logger()
 
-import copy
 import gc
-import glob
-import logging
 import os
-import random
-import sys
 import time
-import weakref
-from typing import Dict, List, Optional
 
-import cv2
-import detectron2.checkpoint as checkpointer
-import detectron2.data as data
-import detectron2.data.transforms as T
-import detectron2.modeling as modeler
-import detectron2.solver as solver
 import detectron2.utils.comm as comm
-import imgaug.augmenters.blur as blur
-import imgaug.augmenters.flip as flip
 
 # import some common libraries
 import numpy as np
 import torch
-from astropy.io import fits
 
 # import some common detectron2 utilities
-from detectron2 import model_zoo
-from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.config import LazyConfig, get_cfg, instantiate
-from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_train_loader
-from detectron2.data import detection_utils as utils
+from detectron2.config import LazyConfig, get_cfg
 from detectron2.engine import (
-    DefaultPredictor,
-    DefaultTrainer,
-    HookBase,
-    SimpleTrainer,
-    default_argument_parser,
-    default_setup,
-    hooks,
     launch,
 )
-from detectron2.engine.defaults import create_ddp_model
-from detectron2.solver import build_lr_scheduler
-from detectron2.structures import BoxMode
-from detectron2.utils.visualizer import Visualizer
 
 from deepdisc.data_format.image_readers import DC2ImageReader
 from deepdisc.data_format.register_data import register_data_set
@@ -73,10 +41,8 @@ from deepdisc.model.loaders import (
     redshift_train_mapper_cls,
     return_test_loader,
     return_train_loader,
-    test_mapper_cls,
-    train_mapper_cls,
 )
-from deepdisc.model.models import RedshiftPDFCasROIHeads, return_lazy_model
+from deepdisc.model.models import return_lazy_model
 from deepdisc.training.trainers import (
     return_evallosshook,
     return_lazy_trainer,
